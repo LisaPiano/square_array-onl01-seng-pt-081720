d26ebe4c1dad292532049b5110dc05ef74f90362
Lisa Walters <campi_l@fortlewis.edu>
2020-08-24 19:30:53 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,5 @@
 def square_array(array)
   new_arr = []
-  array.each { |number| new_arr << number ** 2 }
-  new_arr
-end
-def square_array(array)
-  new_arr = []
-  new_arr.collect{|number| number **2}
+  array.each { |number| new_arr << number ** 2 
   new_arr
 end
